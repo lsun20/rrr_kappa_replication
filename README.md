cb406d6cc2fcfc0c6eb4caa328b4a3350968aac9
Sophie Sun <sophie24sun@gmail.com>
2022-04-27 22:51:34 -0700

    Add AE98 illustration

--- README.md
+++ README.md
@@ -10,10 +10,15 @@ To replicate the simulation studies that compare estimates by DML and kappa weig
 
 To replicate the analysis of the simulation results, execute `plot_dml_rrr.R`, which calculates the bias, MSE, and coverage probability based on simulated estimates.
 
-To replicate the empirical illustration of Auto-DML in the context of effect of 401(k) on assets, weobtain data and construct transformation of the covariates based the replication files for 
+To replicate the empirical illustration of Auto-DML in the context of effect of 401(k) on assets, we obtain data and construct transformation of the covariates based the replication files for 
 Belloni, A., Chernozhukov, V., Fernández-Val, I., and Hansen, C. (2017). Program Evaluation and Causal Inference With High-Dimensional Data. Econometrica 85, 233-298.
 
 We then execute `main_sipp_kappa.R`, which implements the Auto-DML estimator to estimate compliers' counterfactual outcome distribution.  To replicate the plot of the distribution estimates, execute `main_sipp_plot.R`.
+
+To replicate the empirical illustration of Auto-DML in the context of effect of childbearing on female labor supply, we obtain [data](https://economics.mit.edu/faculty/angrist/data1/data/angev98) and construct transformation of the covariates based the [replication files](https://economics.mit.edu/faculty/angrist/data1/data/angrist3) for 
+Angrist, J. D. and Fernández-Val, I.,(2013). ExtrapoLATE-ing: External validity and overidentification in the LATE framework. In Advances in Economics and Econometrics
+
+We then execute `main_AE98_kappa.R`, which implements the Auto-DML estimator to estimate compliers' covariate means.  
 
 ## Authors and acknowledgment
 Rahul Singh, Liyang Sun
---
+++ main_AE98_kappa.R
@@ -0,0 +1,147 @@
+########
+# set up
+########
+
+rm(list=ls())
+install.packages(c('glmnet','grplass','gglasso','plotrix'))
+install.packages(c('glmnet'))
+
+library("foreign")
+library("dplyr")
+library("ggplot2")
+# library("quantreg")
+
+library("MASS")
+library("glmnet")	#lasso, group lasso, and ridge, for outcome models, LPM, mlogit. Also, unpenalized mlogit via glmnet is far faster than the mlogit package.
+# library("grplasso")	#glmnet group lasso requires the same n per group (ie multi-task learning), which is perfect for mlogit but wrong for the outcome model.
+# library("mlogit")	#slower than unpenalized estimation in glmnet, but glmnet won't fit only an intercept
+library("nnet")	#quicker multinomial logit
+library("gglasso")
+#library("plotrix")
+#library("gridExtra")
+#library("randomForest")
+# setwd("~/Desktop/research/rrr_lasso_401k")
+
+#######################
+# clean and format data
+#######################
+
+# source('specifications.R')
+# for (quintile in 1:5){
+source('../specifications_kappa_AE98.R')
+quintile=0 #quintile in (1-5). 0 means all quintiles
+
+print(paste0('quintile: '))
+print(paste0(quintile))
+
+df  <- read.dta("m_d_806_processed.dta")
+#df  <- read.dta("m_d_806_processed_subsample.dta")
+spec=1 #spec in (1-3) # 3 is making mlogit slow
+data<-get_data(df,spec,quintile) #trimming like Farrell; different than Chernozhukov et al. 
+# SS: need to directly edit the script 'specifications_kappa.R' to supplement grid; first column of Y is participation
+
+Y=data[[1]]
+T=data[[2]] #eligibility
+X=data[[3]] #no intercept
+perc=data[[4]] #grid pts
+
+##################
+# helper functions
+##################
+
+source('../primitives.R')
+source('../stage1_kappa.R')
+
+# dictionary
+dict=b2 # b for partially linear model, b2 for interacted model
+p=length(b(T[1],X[1,]))
+
+#p0=dim(X0) used in low-dim dictionary in the stage 1 tuning procedure
+p0=ceiling(p/4) #p/2 for quintile=0. 4 otw
+if (p>60){
+  p0=ceiling(p/40) #p/20 for quintile=0. 40 otw
+}
+p0=2#the minimum number of dict
+
+D_LB=0 #each diagonal entry of \hat{D} lower bounded by D_LB
+D_add=.2 #each diagonal entry of \hat{D} increased by D_add. 0.1 for 0, 0,.2 otw
+max_iter=10 #max number iterations in Dantzig selector iteration over estimation and weights
+
+alpha_estimator=1
+gamma_estimator=1
+
+#alpha_estimator: 0 dantzig, 1 lasso
+#gamma_estimator: 0 dantzig, 1 lasso, 2 rf, 3 nn
+
+###########
+# algorithm
+###########
+
+set.seed(1) # for sample splitting
+
+source('../stage2_kappa.R')
+results<-rrr(Y,T,X,p0,D_LB,D_add,max_iter,dict,alpha_estimator,gamma_estimator) 
+
+theta_hat <- results[[1]]
+j_hat <- results[[2]]
+omega_hat <- results[[3]]
+#moments_full <- results[[4]] 
+psi_hat <- results[[5]] 
+
+# multi2nd instruments
+theta1_hat <- results[[1]]
+j1_hat <- results[[2]]
+omega1_hat <- results[[3]]
+#moments_full <- results[[4]] 
+psi1_hat <- results[[5]] 
+result1 <- results
+
+# samesex instruments
+theta2_hat <- results[[1]]
+j2_hat <- results[[2]]
+omega2_hat <- results[[3]]
+#moments_full <- results[[4]] 
+psi2_hat <- results[[5]] 
+result2 <- results
+
+
+
+# Save multiple objects
+#save(result1, result2, file = "educm_mean_spec1_full.RData")
+save(result1, result2, file = "age2nd_mean_spec1_full.RData")
+
+# To load the data again
+#load("educm_mean_spec1_full.RData")
+load("age2nd_mean_spec1_full.RData")
+
+###########
+# joint confidence bands
+###########
+n <- nrow(X)
+psi_hat <- cbind(psi1_hat,psi2_hat)
+omega_hat <- t(psi_hat) %*% psi_hat  / n
+
+V <- omega_hat/n; V[1,1] <- V[1,1]/j1_hat^2; V[2,2] <- V[2,2]/j2_hat^2
+V[1,2] <- V[1,2]/j1_hat/j2_hat; V[2,1] <- V[2,1]/j1_hat/j2_hat;
+
+
+crit <- 1.96
+theta1_l <- theta1_hat - crit * V[1,1]^0.5
+theta1_u <- theta1_hat + crit * V[1,1]^0.5
+theta2_l <- theta2_hat - crit * V[2,2]^0.5
+theta2_u <- theta2_hat + crit * V[2,2]^0.5
+tstat <- (theta1_hat - theta2_hat)/(V[1,1]-V[1,2]-V[2,1]+V[2,2])^0.5
+
+cbind(theta1_l,theta1_u)
+cbind(theta2_l,theta2_u)
+
+
+V <- j_hat^-1 *omega_hat * j_hat^-1
+theta_hat_var = diag(V)/n
+sigma <- diag(diag(V)^-0.5 ) %*% V %*% diag(diag(V)^-0.5 )
+R <- abs(mvrnorm(10000, rep(0,length(theta_hat)), sigma))
+R_max <- apply(R, 1,FUN=max) # take row-wise max
+crit <- quantile(R_max,0.95)
+crit <- 1.96
+theta_l <- theta_hat - crit * theta_hat_var^0.5
+theta_u <- theta_hat + crit * theta_hat_var^0.5
