cdd47412b5ff5d318423316f7fc53a247376c3d0
Sophie Sun <sophie24sun@gmail.com>
2022-06-04 15:58:23 -0700

    clean up unused specification

--- specifications_kappa_AE98.R
+++ specifications_kappa_AE98.R
@@ -5,35 +5,17 @@ get_data<-function(df,spec,quintile){
   takeup <- df[,"morekids"]
   # Y <- cbind(takeup,outcome) #SS: for LATE just need Y and participation
   Y <- cbind(takeup,outcome*takeup) #SS: for complier covairate just need D*X and participation
-  #SS: create a grid
-  # tausAll <- seq(5,95,5)
-  # pt <- quantile(outcome, tausAll/100)
-  # perc <- unique(pt)
-  # unik <- !duplicated(pt)  ## logical vector of unique values
-  # taus <- tausAll[unik]/100 ## get unique percentile
-  # # taus <- 0 # if not using a grid, set to sth so it goes through
-  # Y <- takeup 
-  # for (i in 1:length(perc)) {
-  #   #SS: create n x d matrix
-  #   Y_entry <- ( outcome <= perc[i] )*takeup #grid for covariate grid (D, D*1{X<=x})
-  #   # Y_entry <- ( outcome <= perc[i] )*takeup #grid for treated ctnr outcome grid (D, D*1{Y<=y})
-  #   # Y_entry <- ( outcome <= perc[i] )*(takeup-1) #grid for untreated ctnr outcome grid (D, (D-1)*1{Y<=y})
-  #   Y <- cbind(Y,Y_entry)
-  # }
   
   #T <- df[,"multi2nd"]
   T <- df[,"samesex"]
   
   ## low dim specification
-  #X.L <- cbind(df[,"educm"], df[,"agem1"], df[,"agefstm"], df[,"boy1st"], df[,"boy2nd"])
   #X.L <- cbind(poly(df[,"educm"], 6, raw=TRUE))
   X.L <- cbind(poly(df[,"ageq2nd"], 6, raw=TRUE))
   ## high dim specification. NOTE: original paper is this spec squared (pairwise interactions thereof?)
   X.H <- cbind(poly(df[,"agem1"], 6, raw=TRUE),
                poly(df[,"agefstm"], 8, raw=TRUE),
                df[,"boy1st"], df[,"boy2nd"]) 
-  # what does original line of code mean?
-  # copied from EJ: "(poly(age, 6, raw=TRUE) + poly(inc, 8, raw=TRUE) + poly(educ, 4, raw=TRUE) + poly(fsize, 2, raw=TRUE) + marr + twoearn + db + pira + hown)^2"
   X.vH=model.matrix(~(poly(df[,"agem1"], 6, raw=TRUE) + 
                         poly(df[,"agefstm"], 8, raw=TRUE) + 
                         poly(df[,"educm"], 4, raw=TRUE) + 
@@ -88,7 +70,7 @@ get_data<-function(df,spec,quintile){
     X.q=X.trimmed
   }
   
-  return(list(Y.q,T.q,X.q,perc))
+  return(list(Y.q,T.q,X.q))
   
 }
 
